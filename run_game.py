f5c22565cca447c87bb6a26ba5e2d7d4073dfff6
s-ter <72151432+s-ter@users.noreply.github.com>
2022-04-11 14:20:32 +0200

    Add files via upload

---
+++ run_game.py
@@ -0,0 +1,19 @@
+from time import sleep
+from game.SpaceInvaders import SpaceInvaders
+from controller.keyboard import KeyboardController
+from controller.random_agent import RandomAgent
+
+def main():
+
+    game = SpaceInvaders(display=True)
+    controller = KeyboardController()
+    #controller = RandomAgent(game.na)
+ 
+    state = game.reset()
+    while True:
+        action = controller.select_action(state)
+        state, reward, is_done = game.step(action)
+        sleep(0.0001)
+
+if __name__ == '__main__' :
+    main()
